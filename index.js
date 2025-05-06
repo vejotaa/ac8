8f3c728271c560acbc7bdd308ea909fa5f9d3c1e
vejotaa <teslawolf@tutanota.com>
2025-05-05 23:17:55 -0300

    cors adicionado

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/api/time', (req, res) => {
   const date = new Date();
